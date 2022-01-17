771f98c2301598284574cc14cdd0b03b72470d20
manish <manishreddynookala@gmail.com>
2022-01-17 16:36:16 +0530

    second commit

--- src/store/actionCreators.ts
+++ src/store/actionCreators.ts
@@ -1,13 +1,5 @@
 import * as actionTypes from "./actionTypes"
 
-// export const  addTransaction = (transaction: ITransaction) => {
-//   const action: TransactionAction = {
-//     type: actionTypes.ADD_TRANSACTION,
-//     payload : transaction,
-//   }
-
-//   return simulateHttpRequest(action)
-// }
 export const addTransaction = (transaction : ITransaction): TransactionAction => ({
   type: actionTypes.ADD_TRANSACTION,
   transaction
@@ -17,19 +9,3 @@ export const removeTransaction = (transaction : ITransaction): TransactionAction
   type: actionTypes.REMOVE_TRANSACTION,
   transaction
 })
-
-// export function removeTransaction(transaction: ITransaction) {
-//   const action: TransactionAction = {
-//     type: actionTypes.REMOVE_TRANSACTION,
-//     transaction,
-//   }
-//   return simulateHttpRequest(action)
-// }
-
-// export function simulateHttpRequest(action: TransactionAction) {
-//   return (dispatch: DispatchType) => {
-//     setTimeout(() => {
-//       dispatch(action)
-//     }, 500)
-//   }
-// }
